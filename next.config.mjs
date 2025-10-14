6989aef810d3009a86b83af589e524269cc41564
inayarhmns <inaya.rahmanisa@gmail.com>
2025-10-14 14:31:31 +0900

    update next config mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    images: {
+    unoptimized: true, // allow Next Image in static mode
+  },
+  assetPrefix: '.', // ensures relative paths for static hosting
+  basePath: '',  
 };
 
 export default nextConfig;
